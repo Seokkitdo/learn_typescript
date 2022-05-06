dcc950f62a852079bb04129ef4df15b9e0030d3b
Seokkitdo <sjd12329@gmail.com>
2022-05-06 19:21:32 +0900

    Add : 함수 타입정의

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,31 @@
+// JS 문자열 선언
+// let str = 'hello';
+
+// TS 문자열 선언
+let str: string = 'hello';
+
+// TS 숫자
+let num: number = 10;
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ['Capt', 'Thor', 10];
+let items: number[] = [1, 2, 3];
+
+// TS 튜플
+let address: [string, number] = ['gangnam', 100];
+
+// TS 객체
+let obj: object = {};
+// let person: object = {
+//     name: 'capt',
+//     age: 100
+// };
+
+let person: {name: string, age: number} = {
+    name: 'Capt',
+    age: 100
+}
+
+// TS 
+let show: boolean = false;
